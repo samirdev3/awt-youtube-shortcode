9ea378520a98927086af8c89e0029636e193a60f
samirdev3 <72462244+samirdev3@users.noreply.github.com>
2021-03-11 17:33:47 +0530

    minor bug fixed

--- options.php
+++ options.php
@@ -1,9 +1,9 @@
 <?php
 /*
    Plugin Name: AWT - Youtube Video Shortcode
-   Plugin URI: https://advweb.in/
-   description: Boost page speed by loading Youtube Video on click using shortcode. [youtube_video id="w3jLJU7DT5E"]
-   Version: 1.0.1
+   Plugin URI: https://github.com/samirdev3/awt-youtube-shortcode
+   description: Boost page/post speed by loading Youtube Video on click using shortcode [youtube_video id="w3jLJU7DT5E"]. Won't work on the homepage.
+   Version: 1.0.2
    Author: Samir
    Author URI: https://github.com/samirdev3
    License: GPL2
@@ -19,7 +19,11 @@ if(! function_exists('awt_yt_video')){
 		$a = shortcode_atts( array(
 			'id' => '',
 		), $atts );
-		return '<div class="yt-container yt-box"><div class="yt-sub-box"><img class="image" src="https://img.youtube.com/vi/'.$a["id"].'/mqdefault.jpg" /><svg class="play-btn" style="width:10%" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m511.914062 256c0-141.386719-114.59375-256-255.957031-256-141.359375 0-255.957031 114.613281-255.957031 256s114.597656 256 255.957031 256c141.363281 0 255.957031-114.613281 255.957031-256zm0 0" fill="#F44336"/><path d="m414.019531 54.628906c34.210938 43.527344 54.617188 98.421875 54.617188 158.085938 0 141.386718-114.59375 256-255.957031 256-59.652344 0-114.539063-20.410156-158.058594-54.628906 46.863281 59.621093 119.628906 97.914062 201.335937 97.914062 141.363281 0 255.957031-114.613281 255.957031-256 0-81.722656-38.285156-154.5-97.894531-201.371094zm0 0" fill="#cc3328"/><path d="m179.933594 362.148438v-200.90625c0-11.878907 13.15625-19.039063 23.128906-12.585938l155.207031 100.453125c9.128907 5.90625 9.128907 19.261719 0 25.171875l-155.207031 100.453125c-9.972656 6.453125-23.128906-.707031-23.128906-12.585937zm0 0" fill="#fff"/><path d="m358.269531 249.109375-155.207031-100.453125c-9.972656-6.453125-23.132812.707031-23.132812 12.585938v7.960937l50.402343 34.6875c41.816407 28.78125 41.011719 90.808594-1.53125 118.496094l-48.871093 31.800781v7.960938c0 11.878906 13.160156 19.039062 23.132812 12.585937l155.207031-100.453125c9.128907-5.90625 9.128907-19.261719 0-25.171875zm0 0" fill="#fff"/></svg></div><iframe class="yt-player" width="560" height="315" data-src="https://www.youtube.com/embed/'.$a["id"].'?autoplay=1&rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div>';
+		if((!is_home() || !is_front_page()) && (is_page() || is_single())){
+			return '<div class="yt-container yt-box"><div class="yt-sub-box"><img class="image" src="https://img.youtube.com/vi/'.$a["id"].'/mqdefault.jpg" /><svg class="play-btn" style="width:10%" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m511.914062 256c0-141.386719-114.59375-256-255.957031-256-141.359375 0-255.957031 114.613281-255.957031 256s114.597656 256 255.957031 256c141.363281 0 255.957031-114.613281 255.957031-256zm0 0" fill="#F44336"/><path d="m414.019531 54.628906c34.210938 43.527344 54.617188 98.421875 54.617188 158.085938 0 141.386718-114.59375 256-255.957031 256-59.652344 0-114.539063-20.410156-158.058594-54.628906 46.863281 59.621093 119.628906 97.914062 201.335937 97.914062 141.363281 0 255.957031-114.613281 255.957031-256 0-81.722656-38.285156-154.5-97.894531-201.371094zm0 0" fill="#cc3328"/><path d="m179.933594 362.148438v-200.90625c0-11.878907 13.15625-19.039063 23.128906-12.585938l155.207031 100.453125c9.128907 5.90625 9.128907 19.261719 0 25.171875l-155.207031 100.453125c-9.972656 6.453125-23.128906-.707031-23.128906-12.585937zm0 0" fill="#fff"/><path d="m358.269531 249.109375-155.207031-100.453125c-9.972656-6.453125-23.132812.707031-23.132812 12.585938v7.960937l50.402343 34.6875c41.816407 28.78125 41.011719 90.808594-1.53125 118.496094l-48.871093 31.800781v7.960938c0 11.878906 13.160156 19.039062 23.132812 12.585937l155.207031-100.453125c9.128907-5.90625 9.128907-19.261719 0-25.171875zm0 0" fill="#fff"/></svg></div><iframe class="yt-player" width="560" height="315" data-src="https://www.youtube.com/embed/'.$a["id"].'?autoplay=1&rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div>';
+		}else{
+			//do nothing
+		}
 	}
 }
 
